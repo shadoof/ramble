16f7c3543d05c91b289b94f2bed3451ef78e3207
dhowe <dhowe@cims.nyu.edu>
2022-01-31 06:49:20 +0800

    implemented ciruclar layout canvas

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 
-let cx = 450, cy = 450, radius = 450,  cursor = 0, layout;
-//fontSize = 32, lineHeight = fontSize * 1.2
+let cx = 475, cy = 475, radius = 460, cursor = 0, minFontSize = 24.4, layout;
 let words = ['by', 'the', 'time', 'the', 'light', 'has', 'faded,', 'as', 'the', 'last', 'of', 'the', 'reddish', 'gold', 'illumination', 'comes', 'to', 'rest,', 'then', 'imperceptibly', 'spreads', 'out', 'over', 'the', 'dust', 'and', 'rubble', 'of', 'the', 'craters', 'on', 'the', 'easterly', 'facing', 'bankside', 'heights,', 'you', 'or', 'I', 'will', 'have', 'rushed', 'out', 'on', 'several', 'of', 'yet', 'more', 'circuits', 'at', 'every', 'time', 'and', 'in', 'all', 'directions,', 'before', 'or', 'after', 'this', 'or', 'that', 'violent,', 'usually', 'nocturnal,', 'event', 'on', 'desperately', 'hurried', 'unfamiliar', 'flights,', 'as', 'if', 'these', 'panics', 'might', 'be', 'movements', 'of', 'desire', 'whereas', 'always', 'our', 'gestures,', 'constrained', 'by', 'obstacles,', 'are', 'also', 'more', 'like', 'scars', 'of', 'universal', 'daily', 'terror:', 'just', 'before', 'or', 'with', 'the', 'dawn,', 'after', 'a', 'morning', 'prayer,', 'in', 'anticipation', 'of', 'hunger,', 'while', 'the', 'neighbors', 'are', 'still', 'breathing,', 'as', 'and', 'when', 'the', 'diligent', 'authorities', 'are', 'marshaling', 'their', 'cronies', 'and', 'thugs,', 'after', 'our', 'own', 'trials', 'of', 'loss,', 'while', 'the', 'mortars', 'still', 'fall,', 'in', 'quiet', 'moments', 'after', 'shock,', 'most', 'particularly', 'after', 'curfew,', 'at', 'sunset,', 'to', 'escape,', 'to', 'avoid', 'being', 'found,', 'to', 'seem', 'to', 'be', 'lost', 'right', 'here', 'in', 'this', 'place', 'where', 'you', 'or', 'I', 'have', 'always', 'wanted', 'to', 'be', 'and', 'where', 'we', 'might', 'sometimes', 'now', 'or', 'then', 'have', 'discovered', 'some', 'singular', 'hidden', 'beauty,', 'or', 'one', 'another,', 'or', 'stumbled', 'and', 'injured', 'ourselves', 'beyond', 'the', 'hearing', 'and', 'call', 'of', 'other', 'voices,', 'or', 'met', 'with', 'other', 'danger,', 'venal', 'or', 'military,', 'the', 'one', 'tearing', 'and', 'rending', 'and', 'opening', 'up', 'the', 'darkness', 'within', 'us', 'to', 'bleed,', 'yet', 'we', 'suppress', 'any', 'sound', 'that', 'might', 'have', 'expressed', 'the', 'terror', 'and', 'longing', 'and', 'horror', 'and', 'pain', 'so', 'that', 'I', 'or', 'you', 'may', 'continue', 'on', 'this', 'expedition,', 'this', 'before', 'or', 'after', 'assault,', 'and', 'still', 'return;', 'or', 'the', 'other,', 'the', 'quiet', 'evacuation', 'of', 'the', 'light,', 'the', 'way,', 'as', 'we', 'have', 'kept', 'on', 'struggling,', 'it', 'falls', 'on', 'us', 'and', 'removes', 'us', 'from', 'existence', 'since', 'in', 'any', 'case', 'we', 'are', 'all', 'but', 'never', 'there,', 'always', 'merely', 'passing', 'through', 'and', 'by', 'and', 'over', 'the', 'dust,', 'within', 'the', 'shadows', 'of', 'our', 'ruins,', 'beneath', 'the', 'wall,', 'within', 'the', 'razor', 'of', 'its', 'coiled', 'wire,', 'annihilated,', 'gone,', 'quite', 'gone,', 'now', 'simply', 'gone', 'and,', 'in', 'being', 'or', 'advancing', 'in', 'these', 'ways,', 'giving', 'up', 'all', 'living', 'light', 'for', 'unsettled,', 'heart', 'felt', 'fire', 'in', 'our', 'veins,', 'exiled'];
 //let txt = "by the time the light has faded, as the last of the reddish gold illumination comes to rest, then imperceptibly spreads out over the dust and rubble of the craters on the easterly facing bankside heights, you or I will have rushed out on several of yet more circuits at every time and in all directions, before or after this or that violent, usually nocturnal, event on desperately hurried unfamiliar flights, as if these panics might be movements of desire whereas always our gestures, constrained by obstacles, are also more like scars of universal daily terror: just before or with the dawn, after a morning prayer, in anticipation of hunger, while the neighbors are still breathing, as and when the diligent authorities are marshaling their cronies and thugs, after our own trials of loss, while the mortars still fall, in quiet moments after shock, most particularly after curfew, at sunset, to escape, to avoid being found, to seem to be lost right here in this place where you or I have always wanted to be and where we might sometimes now or then have discovered some singular hidden beauty, or one another, or stumbled and injured ourselves beyond the hearing and call of other voices, or met with other danger, venal or military, the one tearing and rending and opening up the darkness within us to bleed, yet we suppress any sound that might have expressed the terror and longing and horror and pain so that I or you may continue on this expedition, this before or after assault, and still return; or the other, the quiet evacuation of the light, the way, as we have kept on struggling, it falls on us and removes us from existence since in any case we are all but never there, always merely passing through and by and over the dust, within the shadows of our ruins, beneath the wall, within the razor of its coiled wire, annihilated, gone, quite gone, now simply gone and, in being or advancing in these ways, giving up all living light for unsettled, heart felt fire in our veins, exiled";
 
+let animate = true;
+
 function setup() {
-  createCanvas(900, 900);
+  createCanvas(950, 950);
   textFont('sans-serif');
   textAlign(CENTER, CENTER);
 }
@@ -14,33 +15,53 @@ function draw() {
   background(245);
   noFill();
   circle(cx, cy, radius * 2);
-  if (++cursor <= words.length) {
-    let txt = words.slice(0, cursor).join(' ');
-    layout = maxFontSizeForCircle(txt, radius);
+
+  if (!animate) {
+    layout = circleLayout(words.join(' '), cx, cy, radius, minFontSize);
+    //console.log(layout);
+    noLoop();
   }
+  else {
+    if (++cursor <= words.length) {
+      let txt = words.slice(0, cursor).join(' ');
+      layout = maxFontSizeForCircle(txt, cx, cy, radius);
+      console.log(txt.split(' ').length, 'fontSize:', layout[0].fontSize);
+    }
+    else {
+      console.log('noLoop');
+      noLoop();
+    }
+  }
+  textSize(layout[0].fontSize);
   layout.forEach(r => {
-    noFill();
+    //noFill();
     //rect(...r.bounds);
     fill(0);
-    textSize(r.fontSize);
+    //textSize(r.fontSize);
     text(r.text, cx, r.bounds[1] + r.bounds[3] / 2);
   });
 }
 
+function circleLayout(txt, cx, cy, radius, fontSize, fontName = 'sans-serif') {
+  let result = fitToLineWidths(cx, cy, radius, txt.split(' '), fontSize, fontName);
+  //console.log(result);
+  return result.rects.map((r, i) => ({ fontSize, bounds: r, text: result.texts[i] }));
+}
 
-function maxFontSizeForCircle(text, radius, fontName = 'sans-serif') {
-  let fontSize = radius / 2;
-  let words = text.split(' ');
+function maxFontSizeForCircle(txt, cx, cy, radius, fontName = 'sans-serif') {
+  let fontSize = radius/2;
+  let words = txt.split(' ');
   let result;
   do {
-    result = fitToLineWidths(cx, cy, words, fontSize *= .95);
+    fontSize *= .95;
+    result = fitToLineWidths(cx, cy, radius, words, Math.max(minFontSize, fontSize), fontName);
   }
   while (result.words.length);
 
   return result.rects.map((r, i) => ({ fontSize, bounds: r, text: result.texts[i] }));
 }
 
-function fitToLineWidths(cx, cy, words, fontSize, fontName = 'sans-serif') {
+function fitToLineWidths(cx, cy, radius, words, fontSize, fontName = 'sans-serif') {
   //console.log('fitToLineWidths', fontSize);
   let tokens = words.slice();
   let lh = fontSize * 1.2;
