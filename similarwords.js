35ae3cb4f1a8d5da4fc72d7cdef49e4a261c2389
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2022-05-17 21:04:35 -0400

    Create similarwords.js

---
+++ similarwords.js
@@ -0,0 +1,31 @@
+let words = [
+    { word: "sunset", pos: "nn" },
+    { word: "animal", pos: "jj" },
+    { word: "venal", pos: "jj" },
+    { word: "most", pos: "rbs" },
+    { word: "circadian", pos: "nn" },
+    { word: "violent", pos: "nn" },
+    { word: "simply", pos: "rb" },
+    { word: "will", pos: "md" },
+    { word: "familiar", pos: "jj" },
+    { word: "unfamiliar", pos: "jj" },
+    { word: "mildly", pos: "rb" },
+    { word: "desperately", pos: "rb" },
+    { word: "marshaling", pos: "vbg" },
+    { word: "building", pos: "vbg" },
+    { word: "singular", pos: "jj" },
+    { word: "since", pos: "in" },
+    { word: "beyond", pos: "in" }
+];
+
+let output = {
+    sunset: ['dawning', 'daybreak', 'daylight', 'morning', 'sunrise', 'sunup', 'daytime', 'forenoon', 'crepuscule', 'dusk', 'evening', 'gloaming', 'night', 'nightfall', 'sundown', 'twilight', 'subset', 'inset', 'alphabet', 'mindset', 'quintet'],
+    animal: ['brute', 'beastly', 'brutal', 'brutish', 'bestial', 'fleshly', 'carnal', 'physical', 'bodily', 'corporeal', 'somatic', 'mental', 'nonmaterial', 'bodiless', 'immaterial', 'rational', 'national', 'cardinal', 'minimal', 'intimal', 'animate'],
+    venal: ['corrupt', 'corruptible', 'bribable', 'mercenary', 'avaricious','sordid', 'incorruptible', 'incorrupt', 'renal', 'penal', 'vernal', 'venial', 'genal', 'final', 'viral', 'vital'],
+    most: ['least', 'mostly','inmost', 'must'],
+    circadian: ['rhythms', 'regularity', 'system', 'cycle', 'pattern', 'periodicity', 'radian', 'Mediterranean', 'gradient', 'stadium', 'circassian','orcadian', 'circle', 'cicada'],
+    violent: ['violence', 'brutality', 'fury', 'fierceness', 'ferocity', 'peace', 'calm', 'tranquility', 'quietness', 'silent', 'violet', 'violin'],
+    simply: ['merely', 'basically', 'just', 'smoothly', 'plainly', 'directly', 'easily', 'difficultly', 'hardly', 'barely', 'dimly', 'grimly', 'crisply', 'nimbly', 'primly', 'limply', 'triply', 'steeply'],
+    will: ['shall', 'would', 'ought', 'won\'t', 'wouldn\'t'],
+    familiar: ['conversant', 'acquainted', 'familiarized', 'casual', 'mundane', 'recognizable', 'well-known', 'common', 'intimate', 'unfamiliar', 'unknown', 'unacquainted', 'obscure', 'unusual', 'unmapped', 'similar','family', 'familial', 'female']
+};
